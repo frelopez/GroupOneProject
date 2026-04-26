56dc0478ec9b8eb1fb77c3cd692909a2856f5393
John Username <dstrots@csumb.edu>
2026-04-25 21:10:34 -0700

    FIXED THAT TYPO table creation is all done enough databasemanager methods to demonstrate all CRUD added test folder, started on a test file for databasemanager but it is currently empty until i receive necessary information temporary main method renamed main.java to Main.java because that capitalization WAS relevant

--- src/main/java/DatabaseManager.java
+++ src/main/java/DatabaseManager.java
@@ -7,9 +7,11 @@
  */
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseManager {
-	private static final String DB_URL = "jbdc:sqlite:app.db";
+	private static final String DB_URL = "jdbc:sqlite:app.db";
 	private Connection connection;
 	private static DatabaseManager instance;
 
@@ -41,19 +43,156 @@ public class DatabaseManager {
 	}
 
 	private void createTables() {
-		//check w3schools.com for docs
-		//TODO MORE TABLES
+		//check w3schools.com for docs!
 		String sql = """
-				CREATE TABLE IF NOT EXISTS users(
-				    id		INTEGER	PRIMARY KEY	AUTOINCREMENT,
-				    name	TEXT	NOT NULL,
-				    password	TEXT	NOT NULL
-				    )
+				CREATE TABLE IF NOT EXISTS Users(
+				    id INTEGER PRIMARY KEY AUTOINCREMENT,
+				    name TEXT NOT NULL,
+				    password TEXT NOT NULL,
+				    score INTEGER NOT NULL DEFAULT 0,
+				    peak_score INTEGER NOT NULL DEFAULT 0,
+				    word_0_id INTEGER,
+				    word_1_id INTEGER,
+				    word_2_id INTEGER,
+				    CONSTRAINT UC_User UNIQUE (name),
+				    CONSTRAINT FK_Word0 FOREIGN KEY (word_0_id) REFERENCES Words(id),
+				    CONSTRAINT FK_Word1 FOREIGN KEY (word_1_id) REFERENCES Words(id),
+				    CONSTRAINT FK_Word2 FOREIGN KEY (word_2_id) REFERENCES Words(id)
+				)
 				""";
+
 		try(Statement stmt = connection.createStatement()) {
 			stmt.execute(sql);
 		} catch (SQLException e) {
 			System.err.println("createTables failed: " + e.getMessage());
+		}
+
+		sql = """
+				CREATE TABLE IF NOT EXISTS Words(
+				    id INTEGER PRIMARY KEY AUTOINCREMENT,
+				    word TEXT NOT NULL,
+				    difficulty INTEGER NOT NULL
+				)
+			""";
+
+		try(Statement stmt = connection.createStatement()) {
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			System.err.println("createTables failed: " + e.getMessage());
+		}
+
+		sql = """
+				CREATE TABLE IF NOT EXISTS Attacks(
+				    id INTEGER PRIMARY KEY AUTOINCREMENT,
+				    origin_id INTEGER NOT NULL,
+				    destination_id INTEGER NOT NULL,
+				    word_id INTEGER NOT NULL,
+				    CONSTRAINT FK_origin FOREIGN KEY (origin_id) REFERENCES Users(id),
+				    CONSTRAINT FK_destination FOREIGN KEY (destination_id) REFERENCES Users(id),
+				    CONSTRAINT FK_word FOREIGN KEY (word_id) REFERENCES Words(id)
+				)
+			""";
+
+		try(Statement stmt = connection.createStatement()) {
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			System.err.println("createTables failed: " + e.getMessage());
+		}
+	}
+
+	//TODO maybe return value based on if username already exists?
+	public void insertUser(String name, String password) {
+		String sql = "INSERT INTO Users (name, password) VALUES (?, ?)";
+		try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+			pstmt.setString(1, name);
+			pstmt.setString(2, password);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.err.println("insertion failed: "+e.getMessage());
+		}
+	}
+
+	//TODO redo this once we settle on a concrete implementation
+	public List<String> getAllUsers() {
+		List<String> usernames = new ArrayList<>();
+		String sql = "SELECT name FROM Users ORDER BY id DESC";
+		try(Statement stmt = connection.createStatement();
+		ResultSet rs = stmt.executeQuery(sql)) {
+			while(rs.next()) {
+				usernames.add(rs.getString("name"));
+			}
+		} catch(SQLException e) {
+			System.err.println("getAllUsers failed: "+e.getMessage());
+		}
+		return usernames;
+	}
+
+	//TODO methods to update a user's collected words
+	public void updateUserName(int id, String name) {
+		String sql = "UPDATE Users SET name = ? WHERE id = ?";
+		try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+			pstmt.setString(1, name);
+			pstmt.setInt(2, id);
+			pstmt.executeUpdate();
+		} catch(SQLException e) {
+			System.err.println("updateUserName failed: "+e.getMessage());
+		}
+	}
+
+	public void deleteUser(int id) {
+		String sql = "DELETE FROM Users WHERE id = ?";
+		try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+			pstmt.setInt(1,id);
+			pstmt.executeUpdate();
+		} catch(SQLException e) {
+			System.err.println("deleteUser failed: "+e.getMessage());
+		}
+	}
+
+	//TODO figure out whether this should be temporary
+	public void resetUserTable() {
+		String sql = "DELETE FROM Users";
+		try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+			pstmt.execute();
+		} catch(SQLException e) {
+			System.err.println("resetUserTable failed: "+e.getMessage());
+		}
+	}
+
+	public void insertWord(String word, int difficulty) {
+		String sql = "INSERT INTO Words (word, difficulty) VALUES (?)";
+		try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+			pstmt.setString(1,word);
+			pstmt.setInt(2,difficulty);
+		} catch (SQLException e) {
+			System.err.println("insertion failed: " + e.getMessage());
+		}
+	}
+
+	//TODO figure out how to use foreign keys here. write insertAttack
+	public void insertAttack() {
+		System.out.println("NOT IMPLEMENTED, SILLY!");
+	}
+
+	//TODO ok this one is definitely temporary get rid of it when done using
+	public void dropEverything() {
+		String sql = "DROP TABLE Users";
+		try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+			pstmt.execute();
+		}catch(SQLException e) {
+			System.err.println("FRICK "+e.getMessage());
+		}
+		sql = "DROP TABLE Words";
+		try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+			pstmt.execute();
+		}catch(SQLException e) {
+			System.err.println("FRICK "+e.getMessage());
+		}
+		sql = "DROP TABLE Attacks";
+		try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+			pstmt.execute();
+		}catch(SQLException e) {
+			System.err.println("FRICK "+e.getMessage());
 		}
 	}
 }
