c3469e78a01968cb5fff0a7722871ddb78562e35
frelopez <frelopez@csumb.edu>
2026-04-26 18:07:27 -0700

    Updated Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,11 +8,14 @@ public class Main extends Application {
     private DatabaseManager db;
     @Override
     public void start(Stage stage) {
-        //TODO make a REAL main method
         db = DatabaseManager.getInstance();
         stage.setTitle("Main");
         SceneManager.getInstance().navigateTo(SceneType.MAIN);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
     @Override
     public void stop() {
