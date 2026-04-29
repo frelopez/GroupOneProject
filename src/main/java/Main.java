a05cc5c8bf4f69525c05bad757bb26573bdd433b
dabarker32 <dabarker@csumb.edu>
2026-04-29 15:58:35 -0700

    Add main menu and collection scenes with navigation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,26 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-// to run, type the following line into your terminal!
-    // ./gradlew run
-
     private DatabaseManager db;
+
     @Override
     public void start(Stage stage) {
         db = DatabaseManager.getInstance();
-        stage.setTitle("Main");
+        stage.setTitle("Hang Your Friends, Man");
+
+        SceneManager.init(stage);
         SceneManager.getInstance().navigateTo(SceneType.MAIN);
+
         stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void stop() {
-        if(db!=null) {
+        if (db != null) {
             db.close();
         }
     }
