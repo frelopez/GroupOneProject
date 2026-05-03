6b384a3d6fda708210f3db784b65a81c7985ceb6
frelopez <frelopez@csumb.edu>
2026-05-02 20:12:27 -0700

    Added top users and score from database into the labels of the fxml

--- src/main/java/SendAttackController.java
+++ src/main/java/SendAttackController.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 
 public class SendAttackController {
     @FXML
@@ -41,12 +42,13 @@ public class SendAttackController {
         return scene;
     }
 
-    public void setUpLabels(){
+    public void initialize() {
         DatabaseManager db = DatabaseManager.getInstance();
+        List<String[]> top = db.getTopUsers(3);
 
-        lbl1.setText("Freddie "+db.getUserScore(db.getUserId("Freddie")));
-        lbl2.setText("Angel "+db.getUserScore(db.getUserId("Angel")));
-        lbl3.setText("Dennis "+db.getUserScore(db.getUserId("Dennis")));
+        lbl1.setText(top.get(0)[0] + " " + top.get(0)[1]);
+        lbl2.setText(top.get(1)[0] + " " + top.get(1)[1]);
+        lbl3.setText(top.get(2)[0] + " " + top.get(2)[1]);
     }
 
     public void sendButtonOnAction(ActionEvent actionEvent) {
