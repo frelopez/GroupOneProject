0ad3e4839bd8d5816060ff75bbc568e23057b9f2
frelopez <frelopez@csumb.edu>
2026-05-04 16:49:00 -0700

    Added notification to SendAttackController

--- src/main/java/SendAttackController.java
+++ src/main/java/SendAttackController.java
@@ -1,12 +1,15 @@
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
+
 
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import org.controlsfx.control.Notifications;
 
 import java.io.IOException;
 import java.net.URL;
@@ -29,12 +32,15 @@ public class SendAttackController {
     @FXML
     private Label lbl3;
 
+    private Stage stage;;
+
     private final String[] topUserNames = new String[3];
 
     private final DatabaseManager db = DatabaseManager.getInstance();
 
 
-    public Scene buildScene() {
+    public Scene buildScene(Stage stage) {
+        this.stage = stage;
         URL url = getClass().getResource("SendAttack.fxml");
         FXMLLoader loader = new FXMLLoader(url);
         Scene scene = null;
@@ -64,11 +70,35 @@ public class SendAttackController {
     public void sendButtonOnAction(ActionEvent actionEvent) {
         GameManager gm = GameManager.getInstance();
         if (actionEvent.getSource() == btnSend1) {
+            Notifications.create()
+                    .title("Attack Has Been Sent!")
+                    .text("Your attack has been sent to "+topUserNames[0])
+                    .darkStyle()
+                    .owner(stage)
+                    .position(Pos.TOP_RIGHT)
+                    .showInformation();
             db.insertAttack(gm.getSendAttackWordId(), gm.getUserId(), db.getUserId(topUserNames[0]));
+            db.deleteCollectedWord(gm.getUserId(), gm.getSendAttackWordId());
         }else if (actionEvent.getSource() == btnSend2) {
+            Notifications.create()
+                    .title("Attack Has Been Sent!")
+                    .text("Your attack has been sent to "+topUserNames[1])
+                    .darkStyle()
+                    .owner(stage)
+                    .position(Pos.TOP_RIGHT)
+                    .showInformation();
             db.insertAttack(gm.getSendAttackWordId(), gm.getUserId(), db.getUserId(topUserNames[1]));
+            db.deleteCollectedWord(gm.getUserId(), gm.getSendAttackWordId());
         }else if (actionEvent.getSource() == btnSend3) {
+            Notifications.create()
+                    .title("Attack Has Been Sent!")
+                    .text("Your attack has been sent to "+topUserNames[2])
+                    .darkStyle()
+                    .owner(stage)
+                    .position(Pos.TOP_RIGHT)
+                    .showInformation();
             db.insertAttack(gm.getSendAttackWordId(), gm.getUserId(), db.getUserId(topUserNames[2]));
+            db.deleteCollectedWord(gm.getUserId(), gm.getSendAttackWordId());
         }
         SceneManager.getInstance().navigateTo(SceneType.MAIN);
     }
