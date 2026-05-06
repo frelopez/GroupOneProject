e7fa6ae8fd939abe6695dd8134f7a77df5a1d131
Angel Corrales <acorrales_diaz@csumb.edu>
2026-05-05 19:59:42 -0700

    Created unit test for sign up

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,6 @@
 import javafx.scene.control.Label;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 import javafx.stage.Stage;
@@ -20,19 +22,36 @@ public class LoginTest extends ApplicationTest {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+        SceneManager.init(stage);
+        Parent root = FXMLLoader.load(getClass().getResource("/SignUp.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
     }
-
+    @BeforeEach
+    public void tearDown() {
+        int id = DatabaseManager.getInstance().getUserId("testuser");
+        DatabaseManager.getInstance().deleteUser(id);
+    }
     @Test
     public void testBothFieldsFilledNoError() {
-        clickOn("#username").write("testuser");
-        clickOn("#password").write("testpass");
-        clickOn("#SignInButton");
+        clickOn("#Username").write("testuser");
+        clickOn("#Password").write("testpass");
+        clickOn("#Repassword").write("testpas");
+        clickOn("#SignUpButton");
 
-        Label msg = lookup("#SignInMessage").query();
-        assertEquals("You are signed Up", msg.getText());
+        Label msg = lookup("#label").query();
+        assertEquals("passwords do not match", msg.getText());
+    }
+
+    @Test
+    public void checkdbforuser(){
+        clickOn("#Username").write("testuser");
+        clickOn("#Password").write("testpass");
+        clickOn("#Repassword").write("testpass");
+        clickOn("#SignUpButton");
+
+        String user = GameManager.getInstance().getUser();
+        assertEquals("testuser", user);
     }
 }
 
